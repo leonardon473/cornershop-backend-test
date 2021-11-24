33b4494e18e6e5cfcd3eca26973f0c7fc232153c
Leonardo Ramírez <leonardon473@gmail.com>
2021-11-24 02:12:16 -0600

    added: Menu of the day admin view is added.

--- cornershop-backend-test/backend_test/menu_of_the_day/urls.py
+++ cornershop-backend-test/backend_test/menu_of_the_day/urls.py
@@ -1,2 +1,7 @@
+from django.urls import path
+
+from .views import menu_of_the_day_admin
+
 urlpatterns = [
+    path("admin", menu_of_the_day_admin),
 ]
---
+++ cornershop-backend-test/backend_test/menu_of_the_day/views/__init__.py
@@ -0,0 +1,1 @@
+from .menu_of_the_day_admin import menu_of_the_day_admin  # type: ignore
