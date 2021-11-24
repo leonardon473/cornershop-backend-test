cec9cf41d5c33af8fd6851c58e2904668824120e
Leonardo Ramírez <leonardon473@gmail.com>
2021-11-24 05:07:48 -0600

    added: CRUD is added to Menu of the day.

--- cornershop-backend-test/backend_test/menu_of_the_day/urls.py
+++ cornershop-backend-test/backend_test/menu_of_the_day/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from .views.admin import menu_of_the_day_list_create
-from .views.api import MenuOfTheDayListApiView
+from .views.api import MenuOfTheDayListCreateApiView, MenuOfTheDayRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path("admin", menu_of_the_day_list_create),
-    path("admin/api/menus", MenuOfTheDayListApiView.as_view()),
+    path("admin/menus", menu_of_the_day_list_create),
+    path("admin/menus/<int:id>", menu_of_the_day_list_create),
+    path("admin/api/menus", MenuOfTheDayListCreateApiView.as_view()),
+    path("admin/api/menus/<int:pk>",
+         MenuOfTheDayRetrieveUpdateDestroyView.as_view()),
 ]
---
+++ cornershop-backend-test/backend_test/menu_of_the_day/views/api/menu_of_the_day_list_create.py
@@ -0,0 +1,79 @@
+# -----------------------------------------------------------------------------
+# Libraries
+# -----------------------------------------------------------------------------
+# Core libs
+from typing import TYPE_CHECKING
+
+# Third party libs
+from rest_framework.generics import ListCreateAPIView
+from rest_framework import serializers
+
+# Project libs
+from backend_test.menu_of_the_day.models import FoodDish, MenuOfTheDay
+
+# If type checking, __all__
+if TYPE_CHECKING:
+    from typing import Any, Dict, List
+
+# -----------------------------------------------------------------------------
+# Constants
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Functions
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Classes
+# -----------------------------------------------------------------------------
+
+
+class FoodDishSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FoodDish
+        fields = [
+            'food',
+        ]
+
+
+class MenuOfTheDayCreateSerializer(serializers.ModelSerializer):
+    food_dishes = FoodDishSerializer(many=True)
+
+    class Meta:
+        model = MenuOfTheDay
+        fields = [
+            'date',
+            'food_dishes',
+        ]
+
+    def create(self, validated_data: 'Dict[str, Any]'):
+
+        food_dishes: 'List[Dict[str, Any]]' = validated_data.pop('food_dishes')
+
+        instance: MenuOfTheDay = super().create(validated_data)
+
+        for food_dish in food_dishes:
+            FoodDish.objects.create(
+                menu_of_the_day=instance,
+                **food_dish,
+
+            )
+
+        return instance
+
+    def to_representation(self, instance: MenuOfTheDay):
+        return MenuOfTheDayListSerializer(instance).data
+
+
+class MenuOfTheDayListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuOfTheDay
+        fields = [
+            'id',
+            'date',
+        ]
+
+
+class MenuOfTheDayListCreateApiView(ListCreateAPIView):
+    queryset = MenuOfTheDay.objects.all()
+    serializer_class = MenuOfTheDayCreateSerializer
