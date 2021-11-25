1ef285c3447ceb08693b7fc1f5fb05dec6cd7b04
Leonardo Ramírez <leonardon473@gmail.com>
2021-11-25 16:30:28 -0600

    added: Func get_relation_field is added

---
+++ cornershop-backend-test/backend_test/utils/django/db/models/relation.py
@@ -0,0 +1,41 @@
+# -----------------------------------------------------------------------------
+# Libraries
+# -----------------------------------------------------------------------------
+# Core libs
+from typing import TYPE_CHECKING
+
+# Third party libs
+
+# Project libs
+
+
+# App libs
+
+# If type checking, __all__
+if TYPE_CHECKING:
+    from typing import Optional
+    from django.db.models import Model
+    from django.db.models.fields import Field
+
+# -----------------------------------------------------------------------------
+# Constants
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Functions
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Classes
+# -----------------------------------------------------------------------------
+
+
+def get_relation_field(model_from: 'Model', model_to: 'Model') -> 'Optional[Field]':
+    """
+    Func for find relation field between two models.
+    """
+    for field in model_from._meta.fields:
+        if field.related_model == model_to:
+            return field
+
+    return None
