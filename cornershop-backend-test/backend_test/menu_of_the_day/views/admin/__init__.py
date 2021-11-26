45ea0ede48fe535f6fc782e20b797d44c604091b
Leonardo Ramírez <leonardon473@gmail.com>
2021-11-26 00:32:06 -0600

    style: Flake8 erros are fixed.

--- cornershop-backend-test/backend_test/menu_of_the_day/views/admin/__init__.py
+++ cornershop-backend-test/backend_test/menu_of_the_day/views/admin/__init__.py
@@ -1,2 +1,2 @@
-from .menu_of_the_day_list_create_ import menu_of_the_day_list_create  # type: ignore
-from .menu_of_the_day_retrieve_update_destroy_ import menu_of_the_day_retrieve_update_destroy  # type: ignore
+from .menu_of_the_day_list_create_ import menu_of_the_day_list_create  # type: ignore # noqa
+from .menu_of_the_day_retrieve_update_destroy_ import menu_of_the_day_retrieve_update_destroy  # type: ignore # noqa
--- cornershop-backend-test/backend_test/menu_of_the_day/views/api/__init__.py
+++ cornershop-backend-test/backend_test/menu_of_the_day/views/api/__init__.py
@@ -1,2 +1,2 @@
-from .menu_of_the_day_list_create import MenuOfTheDayListCreateApiView  # type: ignore
-from .menu_of_the_day_retrieve_update_destroy import MenuOfTheDayRetrieveUpdateDestroyView  # type: ignore
+from .menu_of_the_day_list_create import MenuOfTheDayListCreateApiView  # type: ignore # noqa
+from .menu_of_the_day_retrieve_update_destroy import MenuOfTheDayRetrieveUpdateDestroyView  # type: ignore # noqa
