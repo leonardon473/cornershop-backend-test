af9328f85ac94fc8215340d6ac9c2c50848616a7
Leonardo Ramírez <leonardon473@gmail.com>
2021-11-28 21:22:37 -0600

    added: Error management is added to send_menu_of_the_day_to_employees service.

--- cornershop-backend-test/backend_test/menu_of_the_day/services/send_menu_of_the_day_to_employees.py
+++ cornershop-backend-test/backend_test/menu_of_the_day/services/send_menu_of_the_day_to_employees.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import date
 
 from django.conf import settings
@@ -12,21 +13,30 @@ from backend_test.menu_of_the_day.models import (
 )
 
 
+logger = logging.getLogger(__name__)
+
+
 def send_menu_of_the_day_to_employees(menu_date: "date"):
     """
     menu_date: Date of the MenuOfTheDate
     """
-    menu_of_the_day = MenuOfTheDay.objects.get(date=menu_date)
+    try:
+        menu_of_the_day = MenuOfTheDay.objects.get(date=menu_date)
+    except MenuOfTheDay.DoesNotExist:
+        raise ValueError("Menu for date provided doesn't exist")
     for employee in Employee.objects.all():
-        employee_menu_selection = EmployeeMenuSelection.objects.create(
-            employee_id=employee.id, menu_of_the_day=menu_of_the_day
-        )
-        link = f"{settings.BACKEND_HOST}menu/{employee_menu_selection.id}"
-        message = (
-            f"Hola {employee.name} \n"
-            f"En este link {link} puedes "
-            "seleccionar lo que quieras comer el día "
-            f"{menu_of_the_day.date.strftime('%d/%m/%Y')}. \n"
-            "Esperamos lo disfrutes."
-        )
-        send_message_to_slack_user(message, employee.slack_id)
+        try:
+            employee_menu_selection, _ = EmployeeMenuSelection.objects.get_or_create(
+                employee_id=employee.id, menu_of_the_day=menu_of_the_day
+            )
+            link = f"{settings.BACKEND_HOST}menu/{employee_menu_selection.id}"
+            message = (
+                f"Hola {employee.name} \n"
+                f"En este link {link} puedes "
+                "seleccionar lo que quieras comer el día "
+                f"{menu_of_the_day.date.strftime('%d/%m/%Y')}. \n"
+                "Esperamos lo disfrutes."
+            )
+            send_message_to_slack_user(message, employee.slack_id)
+        except Exception as exc:
+            logger.warning(f"Error at send menu to employee {employee.id}: {exc}")
